be8b71d76860b1def1008fcfd445b43c74faee92
Murat Kuzhakhmetov <105972017+Murat1703@users.noreply.github.com>
2022-10-25 14:02:51 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+/*let currentSlide = $('.slider').slick('slickCurrentSlide') + 1;
+let navPage = document.querySelector('.nav-page');
+let SliderCount = document.querySelector('.nav-slider-count');
+SliderCount.text = $(".slider").slick("getSlick").slideCount;
+navPage.text = currentSlide;
+console.log(currentSlide);
+
+$('.single-item').on('afterChange', function(event, slick, currentSlide){
+    console.log(currentSlide + 1);
+ });
+*/
+let CurrentNumber = document.querySelector('.currentCount');
+let TotalCount = document.querySelector('.totalCount');
+
+$(document).ready(function(){
+
+    $('.single-item').slick();
+    var $pagingInfo = $('.paging-info');
+
+    let MycurrentSlide = $('.single-item').slick('slickCurrentSlide') + 1;
+    /**console.log('MyCurrentSlide = ', $(slick.slideCount);*/
+    CurrentNumber.textContent = MycurrentSlide;
+    TotalCount.textContent = $(".single-item").slick("getSlick").slideCount;
+
+    $('.single-item').on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
+        var i = (currentSlide ? currentSlide : 0) + 1;
+       CurrentNumber.textContent = i;
+    });
+});
+
+$('.about-us-slider').slick();
+
+let serviceSpoiler = document.querySelectorAll('.service-item');
+
+serviceSpoiler.forEach(serviceSpoiler => {
+    serviceSpoiler.addEventListener('click', () => {
+        serviceSpoiler.classList.toggle('__active');
+    });    
+});
+
